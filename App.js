6f6c3bde2d90713e812aaa921cd27ee6921aca87
Fikreta <86527560+Firahaket@users.noreply.github.com>
2022-05-16 16:35:48 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import React, { cloneElement } from 'react';
-import { TextInput, View, StyleSheet, Text, ImageBackground, Image, TouchableWithoutFeedbackBase, Dimensions, TouchableOpacity } from 'react-native';
-import { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { cloneElement } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+
+/*
+This JavaScript file introduces navigation and works as a stack for screens
+*/
 
 //screens
-import Login from './screens/Login';
-import Signup from './screens/Signup';
-import Home from './screens/Home';
-import Start from './screens/Start';
-import User from './screens/User';
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+//import Home from "./screens/Home";
+import Start from "./screens/Start";
+import User from "./screens/User";
 
 //variable to define stack for navigation
 const Stack = createNativeStackNavigator();
@@ -18,22 +20,20 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator
+      <Stack.Navigator
         screenOptions={{
           headerStyle: {
-            backgroundColor: 'transparent'
+            backgroundColor: "transparent",
           },
-          headerTransparent: 'true',
-          headerTitle: '',
+          headerTransparent: "true",
+          headerTitle: "",
         }}
-        >
-            <Stack.Screen name="Start" component={Start}/>
-            <Stack.Screen name="Login" component={Login}/>
-            <Stack.Screen name="Signup" component={Signup}/>
-            <Stack.Screen name="User" component={User}/>
-          
-        </Stack.Navigator>
+      >
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="User" component={User} />
+      </Stack.Navigator>
     </NavigationContainer>
-);
+  );
 }
-
