9155c6ebd678f50f64a5e5c0e1b5b566a8b388d9
ilma-k <63539447+ilma-k@users.noreply.github.com>
2022-05-18 12:34:54 +0200

    Add files via upload

--- Lightc.js
+++ Lightc.js
@@ -1,42 +1,57 @@
-import React from "react";
-import LinearGradient from "expo-linear-gradient";
-import CircularProgress from "react-native-circular-progress-indicator";
-import {
-  ImageBackground,
-  Text,
-  StyleSheet,
-  View,
-  Dimensions,
-} from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, View, Dimensions } from "react-native";
 
 import Buttons from "./Buttons";
 import LightIcon from "react-native-vector-icons/Fontisto";
 
-/*
-const temp = {
-  url: "../photos/temp.png",
-};*/
-
+var value = 0;
+var bool;
+if (value == 1) {
+  bool = true;
+} else {
+  bool = false;
+}
 const { width: WIDTH } = Dimensions.get("window");
 const { height: HEIGHT } = Dimensions.get("window");
 
-/***
- * 
-import temp from "../assets/temp.png";
-      <ImageBackground source={temp} style={styles.image}>
-       */
-
 const Lightc = () => {
+  const [shouldShow, setShouldShow] = useState(bool);
+
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
         Light {"  "}
         <LightIcon name="lightbulb" size="30"></LightIcon>
       </Text>
-
-      <View>
-        <Buttons title={"Turn ON"} />
-        <Buttons title={"Turn OFF"} />
+      <View></View>
+      <View style={styles.container}>
+        {shouldShow ? (
+          <Buttons
+            onPress={() => {
+              if (value == 0) {
+                setShouldShow(false);
+                value = 1;
+              } else {
+                setShouldShow(true);
+                value = 0;
+              }
+            }}
+            title={"Turn ON"}
+          />
+        ) : (
+          <Buttons
+            onPress={() => {
+              if (value == 0) {
+                setShouldShow(false);
+                value = 1;
+              } else {
+                setShouldShow(true);
+                value = 0;
+              }
+            }}
+            title={"Turn OFF"}
+          />
+        )}
       </View>
     </View>
   );
@@ -45,11 +60,11 @@ const Lightc = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#C4E3DA",
-    padding: 30,
+    padding: 10,
     borderRadius: 30,
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "row",
+    flexDirection: "column",
     // height: HEIGHT,
     // width: WIDTH - 100,
   },
